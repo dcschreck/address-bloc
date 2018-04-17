fa7a3655e1b91856036b140227aa04ddeab2d9f5
Douglas Schreck <dcschreck421@gmail.com>
2018-04-17 16:01:09 +0300

    Assignment 7: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -46,13 +46,23 @@ class AddressBook
         while lower <= upper
             mid = (lower + upper) / 2
             mid_name = entries[mid].name
-            
+
             if name == mid_name
                 return entries[mid]
             elsif name < mid_name
                 upper = mid - 1
             elsif name > mid_name
                 lower = mid + 1
+            end
+        end
+
+        return nil
+    end
+
+    def iterative_search(name)
+        entries.each do |entry|
+            if entry.name == name
+                return entry
             end
         end
 
